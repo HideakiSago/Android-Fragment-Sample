328f5ef4cba5f43deb7fd087654d1ca66ffdc1da
hideaki.sago <hideaki.sago@gmail.com>
2020-02-11 12:48:04 +0900

    add navigation activity

---
+++ app/src/main/java/jp/hideakisago/androidfragmentsample/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,12 @@
+package jp.hideakisago.androidfragmentsample.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/jp/hideakisago/androidfragmentsample/ui/tools/ToolsViewModel.kt
@@ -0,0 +1,12 @@
+package jp.hideakisago.androidfragmentsample.ui.tools
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ToolsViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is tools Fragment"
+    }
+    val text: LiveData<String> = _text
+}
