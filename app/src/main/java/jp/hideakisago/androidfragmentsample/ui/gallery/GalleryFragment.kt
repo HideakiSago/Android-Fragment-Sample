ef819845f62ad1bb653eb8af289cda89fa4ef2b7
hideaki.sago <hideaki.sago@gmail.com>
2020-02-11 16:34:53 +0900

    Nested Fragment から startActivityForResult を呼び出し、 onActivityResult が正しく呼ばれることを確認。

--- app/src/main/java/jp/hideakisago/androidfragmentsample/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/jp/hideakisago/androidfragmentsample/ui/gallery/GalleryFragment.kt
@@ -1,5 +1,6 @@
 package jp.hideakisago.androidfragmentsample.ui.gallery
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,10 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import jp.hideakisago.androidfragmentsample.Feature1Activity
 import jp.hideakisago.androidfragmentsample.R
+import kotlinx.android.synthetic.main.fragment_gallery.*
+import timber.log.Timber
 
 class GalleryFragment : Fragment() {
     private lateinit var galleryViewModel: GalleryViewModel
@@ -26,5 +30,17 @@ class GalleryFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        text_gallery.setOnClickListener {
+            startActivityForResult(Intent(requireContext(), Feature1Activity::class.java), 1)
+        }
+    }
+
+    // onActivityResult on Nested Fragment
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Timber.d("onActivityResult action:${data?.action} data:${data?.data} requestCode:$requestCode resultCode:$resultCode")
     }
 }
