2048e68e4d70f4a4065c127d59888e3e4b0405f4
hideaki.sago <hideaki.sago@gmail.com>
2020-02-11 12:39:01 +0900

    format all

--- app/src/test/java/jp/hideakisago/androidfragmentsample/ExampleUnitTest.kt
+++ app/src/test/java/jp/hideakisago/androidfragmentsample/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package jp.hideakisago.androidfragmentsample
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
